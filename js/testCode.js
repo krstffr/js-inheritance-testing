44d706dd1107dfe497a12753035aac4016537ec9
Kristoffer Klintberg <krstffr@gmail.com>
2013-09-24 11:28:01 +0200

    Setting same "tests" for all branches

--- js/testCode.js
+++ js/testCode.js
@@ -1,28 +1,38 @@
-var someHuman = new Human();
-console.log('Has this thing got a mouth on it? '+someHuman.hasMouth);
-console.log(someHuman.makeNoise('I am not a god, but a human'));
-console.log(someHuman.makeTalk('I suck'));
-console.log('---');
-
 var someMammal = new Mammal();
 console.log('Has this thing got a mouth on it? '+someMammal.hasMouth);
 console.log(someMammal.makeNoise());
 console.log('---');
 
+var someHuman = new Human();
+console.log('Has this thing got a mouth on it? '+someHuman.hasMouth);
+console.log(someHuman.makeNoise());
+console.log(someHuman.makeTalk('I suck'));
+console.log('---');
+
 var someSoccerPlayer = new SoccerPlayer();
 console.log('Has this thing got a mouth on it? '+someSoccerPlayer.hasMouth);
 console.log(someSoccerPlayer.makeNoise());
-console.log(someSoccerPlayer.makeTalk('soccer rules!'));
-console.log('My random soccer skill is: '+someSoccerPlayer.soccerSkill);
+console.log(someSoccerPlayer.makeTalk('I am decent at football'));
 console.log(someSoccerPlayer.makeSoccerShot());
+console.log(someSoccerPlayer.name+'\'s soccer skill is: '+someSoccerPlayer.soccerSkill);
+console.log('---');
+
+var someWizard = new Wizard();
+console.log('Has this thing got a mouth on it? '+someWizard.hasMouth);
+console.log(someWizard.makeNoise());
+console.log(someWizard.makeTalk('I am magic'));
+console.log(someWizard.makeMagic());
+console.log('Magic power is: '+someWizard.magicLevel);
+console.log('Has this thing got a mouth on it? '+someWizard.hasMouth);
 console.log('---');
 
 var messi = new Messi();
 console.log('Has this thing got a mouth on it? '+messi.hasMouth);
 console.log(messi.makeNoise());
-console.log(messi.makeTalk('soccer rules!'));
-console.log('My random soccer skill is: '+messi.soccerSkill);
+console.log(messi.makeTalk('I am decent at football'));
 console.log(messi.makeSoccerShot());
+console.log(messi.name+'\'s soccer skill is: '+messi.soccerSkill);
 console.log(messi.makeMagic());
-console.log('Still has a mouth on it? '+messi.hasMouth);
+console.log('Magic power is: '+messi.magicLevel);
+console.log('Has this thing got a mouth on it? '+messi.hasMouth);
 console.log('---');
